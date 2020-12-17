2ec61e5d53003ba4f3a4c8f5ee078952de41818b
chepecarlos <chepecarlos@alswblog.org>
2020-12-17 15:49:16 -0600

    Codigo Base de Arduino

--- Arduino/ArbolNavidad2020/ArbolNavidad2020.ino
+++ Arduino/ArbolNavidad2020/ArbolNavidad2020.ino
@@ -1,7 +1,37 @@
 void setup() {
-  
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  
+  if (Serial.available()) {
+    DecodificarSerial();
+  }
+}
+
+
+void DecodificarSerial() {
+  // Mensaje de la forma M1/30
+  // Donde M1 Es motor 1
+  // 30 el valor
+  String Mensaje = Serial.readStringUntil('\n');
+  int PosicionPleca = Mensaje.indexOf('/');
+  int PosicionSaltoLinea = Mensaje.length();
+  String Dato = Mensaje.substring(0, PosicionPleca);
+  String Valor = Mensaje.substring(PosicionPleca + 1, PosicionSaltoLinea);
+  //.toInt();
+  //  if (Dato.equals("M1")) {
+  //    M1 = Valor;
+  //  }
+  //  else if (Dato.equals("M2")) {
+  //    M2 = Valor;
+  //  }
+
+
+  Serial.print("Dato:");
+  Serial.print(Dato);
+  Serial.print(" Valor:");
+  Serial.print(Valor);
+  Serial.println();
+
 }
